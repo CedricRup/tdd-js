fba12473dc8a091dbc5bb3a659f0a2a090d4077d
Cedric Rup <cedric.rup@gmail.com>
2017-02-01 08:39:29 +0100

    jQuery

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 //Load common code that includes config, then load the app logic for this page.
-requirejs(['common/glop'], function (glop) {
-    document.getElementById("demo").innerHTML = glop.hello();
+requirejs(['common/glop','lib/jquery'], function (glop) {
+    $("#demo").text(glop.hello());
 });
